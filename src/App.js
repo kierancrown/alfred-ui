f528dfb16c541055b56aa8a940c80ea6b934987d
Kieran Crown <kieran.crown@gmail.com>
2020-11-25 23:55:27 +0000

    Added test button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
+import Axios from "axios";
 import "./App.css";
 
 function App() {
+  const [testState, setTestState] = useState(false);
+
+  const test = () => {
+    setTestState(!testState);
+    Axios.put(
+      "/hue/light",
+      JSON.stringify({ id: 1, state: { on: testState } })
+    );
+  };
+
   return (
     <div className="App">
       <h1>Alfred</h1>
+      <button onClick={test}>Turn {testState ? "Off" : "On"} Light</button>
     </div>
   );
 }
