b7a476c44a48d2b3a388ba39e248f123cabb114a
Kieran Crown <kieran.crown@gmail.com>
2020-11-26 00:33:26 +0000

    Added brightness slider

--- src/App.js
+++ src/App.js
@@ -4,20 +4,68 @@ import "./App.css";
 import Config from "./constants";
 
 function App() {
-  const [testState, setTestState] = useState(false);
+  const [lightState, setLightState] = useState(false);
+  const [lightBri, setLightBri] = useState(254);
 
-  const test = () => {
-    setTestState(!testState);
-    Axios.put(`${Config.base_url}hue/light`, {
-      id: 1,
-      state: { on: testState },
-    });
+  const changeLightState = () => {
+    setLightState(!lightState);
+    Axios.put(`${Config.base_url}hue/light`, [
+      {
+        id: 1,
+        state: { on: lightState },
+      },
+      {
+        id: 2,
+        state: { on: lightState },
+      },
+    ]);
+  };
+
+  const updateLightBrightness = () => {
+    if (lightBri < 1) {
+      Axios.put(`${Config.base_url}hue/light`, [
+        {
+          id: 1,
+          state: { on: false },
+        },
+        {
+          id: 2,
+          state: { on: false },
+        },
+      ]);
+      return;
+    }
+    setLightState(true);
+    Axios.put(`${Config.base_url}hue/light`, [
+      {
+        id: 1,
+        state: { on: true, bri: Number(lightBri) },
+      },
+      {
+        id: 2,
+        state: { on: true, bri: Number(lightBri) },
+      },
+    ]);
   };
 
   return (
     <div className="App">
-      <h1>Alfred</h1>
-      <button onClick={test}>Turn {testState ? "Off" : "On"} Light</button>
+      <h1>Alfred Test</h1>
+      <p>This will control the bedroom lights</p>
+      <button onClick={changeLightState}>
+        Turn {!lightState ? "Off" : "On"} All Bedroom Lights
+      </button>
+      <input
+        type="range"
+        min={0}
+        max={254}
+        value={lightBri}
+        onChange={(e) => {
+          setLightBri(e.currentTarget.value);
+        }}
+        onMouseUp={updateLightBrightness}
+        onKeyUp={updateLightBrightness}
+      />
     </div>
   );
 }
